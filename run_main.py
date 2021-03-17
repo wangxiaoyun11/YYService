423e0e989c423387fd97bcd88403ddf846144dcd
wangxy <944378670@qq.com>
2021-03-17 14:48:35 +0800

    框架修改

--- run_main.py
+++ run_main.py
@@ -1,13 +1,2 @@
-
-def quick_sort(list):
-
-    for i in range(0,len(list)):
-        for j in (i+1,len(list)):
-            if list[i]==list[j]:
-                list.pop(i) and list.pop(j)
-                return list
-
-
-
-
-print(quick_sort([1,23,4,2,33,33,1]))
+dict1 = {'a' : 1, 'b' : 2}
+print(dict1.pop("a"))
